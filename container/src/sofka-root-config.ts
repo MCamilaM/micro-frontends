98c66558f51beee9f3c0228a646dc9dd349c2a55
pilar <ingeniera.coral@gmail.com>
2022-06-17 12:44:11 -0500

    pokemon poncomente -- agregando componenete pokemon con angular

--- container/src/sofka-root-config.ts
+++ container/src/sofka-root-config.ts
@@ -1,17 +1,21 @@
 import { registerApplication, start } from "single-spa";
 
-
 registerApplication({
   name: "@sofka/mf-footer",
   app: () => System.import("@sofka/mf-footer"),
-  activeWhen: ["/angular-app"]
+  activeWhen: ["/angular-app"],
 });
 
+registerApplication({
+  name: "@sofka/mf-footer2",
+  app: () => System.import("@sofka/mf-footer2"),
+  activeWhen: ["/angular2-app"],
+});
 
 registerApplication({
   name: "@sofka/header",
   app: () => System.import("@sofka/header"),
-  activeWhen: ["/react-app"]
+  activeWhen: ["/react-app"],
 });
 
 start({
